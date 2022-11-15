9f7518af7ff78fe5cdf55ac0168e68bb6770dda6
Pablo <p.reinaldo90@gmail.com>
2022-11-15 19:05:07 +0100

    final commit

--- Calculo IVA/app.js
+++ Calculo IVA/app.js
@@ -2,55 +2,62 @@
  libro 4%
  resto 21%*/
 
- const product = { count:3, price: 12.5, type:"ropa"}
+const product = { count: 3, price: 12.5, type: "ropa" }
 
 //Calculamos el precio total, cantidad de productos por su precio
- let total
+let total
 
 
- if (product.count > 0)  { 
-    total=product.count *product.price;
+if (product.count > 0) {
+    total = product.count * product.price;
 }
 
-else{
+else {
     total <= 0
 }
 
-console.log("El precio bruto es"+ " "+ total);
+console.log("El precio bruto es" + " " + total);
 
 //Ahora  procedemos a calcular el IVA dependiendo del tipo de producto con un switch
 
 let iva;
 
 
-switch(product.type){
+switch (product.type) {
     case "alimentacion":
-        iva  = total * 0.10; break;
+        iva = total * 0.10; break;
     case "libro":
         iva = total * 0.04; break;
 
-    default: iva= total* 0.21;
+    default: iva = total * 0.21;
 
 }
-console.log("El iva es"+" "+iva);
+console.log("El iva es" + " " + iva);
 
 //Sumamos el iva al precio total
 
-let priceTot = total+iva
+let priceTot = total + iva
 
-console.log ("El precio total es"+" " +priceTot);
+console.log("El precio total es" + " " + priceTot);
 
-//iva+product.price*product.count
+//Calculadora Avanzada
 
+let ivaF;
 
-
-
-function getVat(product){
-  product.price+iva
+if (product.type == "alimentacion") {
+    ivaF = 1.1;
+} else if (product.type == "libro") {
+    ivaF = 1.04;
+} else {
+    ivaF = 1.21;
 }
 
-function getTotalVat(product) {
-    return product.count > 0 ? product.count * getVat(product) : 0;
-  }
 
-  console.log(getTotalVat)
+function getVat() {
+    return product.price * ivaF
+}
+
+function getTotalVat() {
+    return product.count > 0 ? product.count * getVat(product) : 0;
+}
+console.log(getTotalVat())
